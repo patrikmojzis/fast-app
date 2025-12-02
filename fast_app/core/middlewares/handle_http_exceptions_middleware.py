1d15c10b2b581c92fb0c8103c47cc2b45eee6981
patrikmojzis <38838813+patrikmojzis@users.noreply.github.com>
2025-12-02 22:10:14 +0100

    fix unlogged exceptions

--- fast_app/core/middlewares/handle_http_exceptions_middleware.py
+++ fast_app/core/middlewares/handle_http_exceptions_middleware.py
@@ -1,9 +1,12 @@
+import logging
 import os
 from typing import Any, Callable, Awaitable
 
 from fast_app.contracts.middleware import Middleware
-from fast_app.exceptions.common_exceptions import AppException
 from fast_app.exceptions import HttpException, ServerErrorException, ModelException
+from fast_app.exceptions.common_exceptions import AppException
+
+logger = logging.getLogger(__name__)
 
 
 class HandleHttpExceptionsMiddleware(Middleware):
@@ -17,12 +20,14 @@ class HandleHttpExceptionsMiddleware(Middleware):
         except HttpException as e:
             return e.to_response()
         except AppException as e:
+            logger.exception("Application exception while handling request", exc_info=e)
             if os.getenv("ENV") == "debug":
                 raise e
 
             return e.to_response()
         except Exception as e:
+            logger.exception("Unhandled exception while handling request", exc_info=e)
             if os.getenv("ENV") == "debug":
                 raise e
-                
+
             return ServerErrorException().to_response()
